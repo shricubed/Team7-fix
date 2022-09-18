008e3df0380578557757cb85adb73d56036745e0
Josh Wyatt <josh.wyatt197@gmail.com>
2022-09-18 14:29:24 -0400

    first attempt at eatCookie

--- Projects/P3/src/Map.java
+++ Projects/P3/src/Map.java
@@ -69,7 +69,41 @@ public class Map {
 
   public JComponent eatCookie(String name) {
     // update locations, components, field, and cookies
-    // the id for a cookie at (10, 1) is tok_x10_y1
-    return null;
+    // the id for a cookie at (10, 1) is tok_x10_y1, id is a string
+
+    /* grabs pacmans current location by referencing name from locations hashmap
+     * checks if there is also a cookie at that location, by using the location grabbed
+     * and checking the fields hashmap, one coordinate could lead to multiple types
+     * ex, field.get(Loc(10, 4)) could return {PACMAN, COOKIE, GHOST} 
+     * if a cookie exists here, remove it from that field
+     * and return the cookie that was eaten
+     * also update display?
+     * 
+     */
+    
+    //pacman location
+    Location pac_man_loc = locations.get(name);
+    //getting other components at that locations
+    HashSet<Type> other_comp = field.get(pac_man_loc);
+
+    if(other_comp.contains(Type.COOKIE) && other_comp.contains(Type.PACMAN)){
+      //making sure there is a cookie, and pacman at this location
+      String cookie_name = "tok_x" + pac_man_loc.x + "_y" + pac_man_loc.y;
+      //removing the cookie from locations
+      locations.remove(cookie_name);
+      //storing return value of cookie eaten
+      JComponent eaten_cookie =  components.get(cookie_name);
+      //removing cookie from components
+      components.remove(cookie_name);
+      //removing cookie enum from field
+      field.get(pac_man_loc).remove(Type.COOKIE);
+      //incrementing cookies eaten
+      cookies += 1;
+      //simply return the cookie now
+      return eaten_cookie;
+    }else{
+      //no cookie here
+      return null;
+    }
   }
 }
