ee3667aec7b7e492dd90edcd4ca2e0b69829d18e
Roshan Shet <roshanshet1@gmail.com>
2022-09-20 13:01:56 -0400

    get moves for pacman

--- Projects/P3/src/PacMan.java
+++ Projects/P3/src/PacMan.java
@@ -14,7 +14,16 @@ public class PacMan {
   }
 
   public ArrayList<Location> get_valid_moves() {
-    return null;
+	  ArrayList<Location> list = new ArrayList<Location>();
+	  for (int i = -1; i < 2; i++) {
+		  for (int j = -1; j < 2; j++) {
+			  Location loc = new Location(myLoc.x + i, myLoc.y + j);
+			  if ((i != 0 || j != 0) && myMap.getLoc(loc).contains(Map.Type.EMPTY)) {
+				  list.add(loc);
+			  }
+		  }
+	  }
+	  return list;
   }
 
   public boolean move() {
