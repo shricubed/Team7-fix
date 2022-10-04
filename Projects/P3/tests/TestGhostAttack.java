556cf2e1530a149680df3ead96ff4a1efda3da9e
Josh Wyatt <josh.wyatt197@gmail.com>
2022-10-04 18:47:37 -0400

    further ghostAttack test implementation

--- Projects/P3/tests/TestGhostAttack.java
+++ Projects/P3/tests/TestGhostAttack.java
@@ -13,13 +13,15 @@ public class TestGhostAttack extends TestCase {
 
     //Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red); //Creates a red ghost named "name" at location x,y
     PacMan pacman = frame.addPacMan(new Location(2, 3)); //Creates PacMan at location x, y 2,3 is a cookie so is 2,3
-    Ghost test = frame.addGhost(new Location(2, 3), "test", Color.red);
+    Ghost test = frame.addGhost(new Location(2, 4), "test", Color.red);
     HashSet<Map.Type> loc_items = frame.getMap().getLoc(new Location(2, 3));
+    System.out.println("Ghosts location before attack");
+    System.out.println(frame.getMap().getLoc(new Location(2, 4)));
     HashSet<Map.Type> expected = new HashSet<Map.Type>();
     expected.add(Map.Type.COOKIE);
     expected.add(Map.Type.PACMAN);
     //expected.add(Map.Type.GHOST);
-    System.out.println("Asserting that pacman and cookie and ghost are present at location");
+    System.out.println("Asserting that pacman and cookie are present at location");
     assertEquals(loc_items, expected);
     JComponent temp = pacman.consume();
     System.out.println("Asserting that cookie was consumed");
@@ -31,9 +33,13 @@ public class TestGhostAttack extends TestCase {
     assertTrue(result);
     HashSet<Map.Type> after_consume = frame.getMap().getLoc(new Location(2, 3));
     expected = new HashSet<Map.Type>();
-    expected.add(Map.Type.PACMAN);
-    System.out.println("Asserting that old cookie was removed from the current location");
+    expected.add(Map.Type.GHOST);
+    System.out.println("Asserting that cookie was consumed, ghost moved into location, pacman removed since attacked");
     assertEquals(after_consume, expected);
-    System.out.println(frame.getMap().isGameOver());
+    System.out.println("Asserting that game should be considered over since pacman was attacked");
+    assertTrue(frame.getMap().isGameOver());
+
+    System.out.println("Ghosts old location after attack");
+    System.out.println(frame.getMap().getLoc(new Location(2, 4)));
   }
 }
